c57e03255a77e69d3dc86118a81141ffe975fb44
LumberjackIncorperated <lumerberjacksincorperated@MacBook-Pro-4.local>
2018-06-26 12:56:20 +1000

    Cleaned up conditions and added explanation file

--- conditions.py
+++ conditions.py
@@ -11,7 +11,7 @@ import types
 testFlag = None
 
 #------------------------------------------------------------------------------------------------------
-# IMPLEMENTATION
+# AUXILLARY CLASSES
 #------------------------------------------------------------------------------------------------------
 class PreconditionError(Exception):
     '''
@@ -23,40 +23,55 @@ class ArguementError(Exception):
     This exception is given when the precondition tests given in the precondition decorator are invalid
     '''
 
-def preconditions(*preconditions):
-    '''
-    DESCRIPTION:
-        Takes a list of preconditions as lambda expressions, and decorates a function in a check
-        of these preconditions on the applied function arguements at runtime
-
-    PARAMETERS:
-        *preconditions: A tuple containing lambda expressions representing preconditions for the
-                        wrapped function's arguements
-
-    RETURNS:
-        (valid arguement)
-            Returns the function that is wrapped in a check of the preconditions
-            when applied to arguements
-        (invalid arguement)
-            An ArguementError is raised
-    '''
+#------------------------------------------------------------------------------------------------------
+# SUPPORT FUNCTIONS
+#------------------------------------------------------------------------------------------------------
+def _check_preconditions_are_all_lambda_expressions(preconditions):
     for precondition in preconditions:
         if not isinstance(precondition, types.LambdaType):
             raise ArguementError
 
+def _check_precondition_for_arguement(precondition, arg):
+    try:
+        assert precondition(arg)
+    except AssertionError:
+        raise PreconditionError()  #print "PreconditionError: with arguement number[", i, "] in function:", function.__name__, "with arg:", args[i]
+
+def _perform_precondition_check_on_arguements(preconditions, args):
+    for preconditionIndex in range(len(preconditions)):
+        if (preconditions[preconditionIndex] is not None): #and (args[i] is not None):
+            _check_precondition_for_arguement(preconditions[preconditionIndex], args[preconditionIndex])
+
+def _wrap_function_in_precondition_check_with_preconditions_and_args(function, preconditions, args):
+    _perform_precondition_check_on_arguements(preconditions, args)
+    return function(*args)
+
+def _wrap_function_in_precondition_decorator(preconditions):
     def decorator(function):
         def function_wrapper(*args):
-            for i in range(len(preconditions)):
-            	if (preconditions[i] is not None): #and (args[i] is not None):
-            	    try:
-            	    	assert preconditions[i](args[i])
-            	    except AssertionError:
-            	        #print "PreconditionError: with arguement number[", i, "] in function:", function.__name__, "with arg:", args[i]
-            	    	raise PreconditionError()
-            result = function(*args)
-            return result
+            return _wrap_function_in_precondition_check_with_preconditions_and_args(function, preconditions, args)
         return function_wrapper
     return decorator
+
+#------------------------------------------------------------------------------------------------------
+# IMPLEMENTATION
+#------------------------------------------------------------------------------------------------------
+def preconditions(*preconditions):
+    '''
+    DESCRIPTION:
+        Applies preconditions to a function or method
+
+    PARAMETERS:
+        *preconditions: A tuple containing lambda expressions that represent the preconditions
+
+    RETURNS:
+        (valid arguement)
+            A function that has been wrapped with the preconditions
+        (invalid arguement)
+            An ArguementError is raised
+    '''
+    _check_preconditions_are_all_lambda_expressions(preconditions)
+    return _wrap_function_in_precondition_decorator(preconditions)
 
 #------------------------------------------------------------------------------------------------------
 # TESTING IMPLEMENTATION
@@ -66,6 +81,7 @@ class TestPrecondition(unittest.TestCase):
     #------------------------------------------------------------------------------------------------------
     # TESTING SUPPORT CODE
     #------------------------------------------------------------------------------------------------------
+    
     ''' Test functions designed to test out different sets of valid preconditions '''
     @preconditions(     (lambda arg1: arg1 > 2)
                     and (lambda arg2: arg2 > 2)
@@ -78,12 +94,9 @@ class TestPrecondition(unittest.TestCase):
     def dummyFunctionTwo(arg1, arg2):
         return True
 
-    ''' Tuples containing the pairs of test functions and aguements for these test functions '''
+    ''' Tuples containing the pairs of test functions and arguements for these test functions '''
     testFunctionPositiveApplicationList = ((dummyFunctionOne, (3, 3, 3)), (dummyFunctionTwo, (30,30)))
     testFunctionNegativeApplicationList = ((dummyFunctionOne, (1, 3, 3)), (dummyFunctionTwo, (2,30)))
-
-    def setUp(self):
-        pass
  
     #------------------------------------------------------------------------------------------------------
     # POSITIVE TESTING
